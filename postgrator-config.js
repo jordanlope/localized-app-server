399fa6662cd82e8033455e9d7ab45cd794f79d73
Jordan Lopez <jordanlopez992@gmail.com>
2020-09-29 22:49:56 -0400

    Back-end deploy fixes

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,6 +4,4 @@ module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
     "connectionString": "postgresql://jordanlopez992@localhost/localized_server",
-    // (config.env.NODE_ENV === 'test')
-    // ? process.env.TEST_DATABASE_URL
 }
--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+
+
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: "postgresql://jordanlopez992@localhost/localized_server",
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://jordanlopez992@localhost/localized_server",
 }
