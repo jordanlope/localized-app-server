1871df3162146fc9a27ea668ec380435f4b2309c
Jordan Lopez <jordanlopez992@gmail.com>
2020-10-04 17:59:00 -0400

    Config fix

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,7 @@
-//const config = require('./src/config')
+const config = require('./src/config')
 
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    "connectionString": "postgres://latjpnpngybyjc:89cafb833533c6365414f754363b3ccdb3b100daaf173d3ab828aa247be11922@ec2-3-224-97-209.compute-1.amazonaws.com:5432/ddes2tnp3cvilv",
+    "connectionString": config.DATABASE_URL,
 }
--- src/config.js
+++ src/config.js
@@ -1,8 +1,6 @@
-
-
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://jordanlopez992@localhost/localized_server",
-    JWT_SECRET: "Jordanlopez992"
+    JWT_SECRET: process.env.JWT_SECRET
 }
